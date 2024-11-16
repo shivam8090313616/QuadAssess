c53b5ee4e237cabb8afd3e8a353c09075fe01c9a
SHIVAM PRAJAPATI <132261681+quadcode-shivam@users.noreply.github.com>
2024-11-16 21:26:31 +0530

    wser

--- database/migrations/2024_11_14_093245_create_interview_user_data_table.php
+++ database/migrations/2024_11_14_093245_create_interview_user_data_table.php
@@ -6,21 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-   
     public function up(): void
     {
         Schema::create('interview_user_data', function (Blueprint $table) {
             $table->id();
             $table->string('interview_id')->nullable(); // VARCHAR for interview_id, nullable
+            $table->string('email')->unique()->nullable();
+            $table->string('experienceLevel')->nullable();
+            $table->string('name')->nullable();
+            $table->string('role')->nullable();
+            $table->integer('status')->nullable()->comment("1: inprogress, 2: complete, 3: not-check, 4: checked");
             $table->json('skills')->nullable();         // JSON datatype for skills, nullable
             $table->json('marks_per_question')->nullable(); // JSON datatype for marks_per_question, nullable
             $table->integer('score')->nullable();
             $table->integer('correct_answers')->nullable();
             $table->integer('incorrect_answers')->nullable();
-            $table->string('email')->unique()->nullable();
-            $table->string('experienceLevel')->nullable();
-            $table->string('name')->nullable();
-            $table->string('role')->nullable();
             $table->timestamps(); 
         });
     }
